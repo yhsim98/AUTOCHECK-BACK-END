75de49dfe565161d783a88b70981da94b0e48743
yhsim98 <2008qwe123@gmail.com>
2022-02-25 02:36:44 +0900

    Feature/img-upload

--- src/main/java/com/auto/check/service/RegistrationService.java
+++ src/main/java/com/auto/check/service/RegistrationService.java
@@ -1,6 +1,5 @@
 package com.auto.check.service;
 
-import com.auto.check.domain.attendance.Attendance;
 import com.auto.check.domain.lecture.Lecture;
 import com.auto.check.domain.registration.Registration;
 import com.auto.check.domain.registration.RegistrationRepository;
@@ -8,7 +7,6 @@ import com.auto.check.domain.user.User;
 import com.auto.check.enums.ErrorMessage;
 import com.auto.check.exception.NonCriticalException;
 import lombok.RequiredArgsConstructor;
-import lombok.var;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,8 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistrationService {
 
     private final RegistrationRepository registrationRepository;
-    private final UserService userService;
-    private final LectureService lectureService;
 
     public boolean isRegistration(User user, Lecture lecture){
         if(registrationRepository.countByStudentIdAndLectureId(user.getId(), lecture.getId()) < 1)
